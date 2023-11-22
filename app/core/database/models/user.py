a6fae90e428ed0f96f74ff20377186a4f3ff98a7
broccoli <vovo.r@ya.ru>
2023-11-22 17:58:18 +1000

    create model Users for db

--- app/core/database/models/user.py
+++ app/core/database/models/user.py
@@ -1,4 +1,14 @@
-class User:
-	pass
+from sqlalchemy import Column, Integer, String
+from app.core.database import Base
 
-__all__ = ['User']
+class Users(Base):
+	__tablename__ = 'users'
+	id = Column(Integer, primary_key=True, autoincrement=True)
+	surname = Column(String(50), nullable=False)
+	name = Column(String(50), nullable=False)
+	patronymic = Column(String(50), default=None)
+	login = Column(String, nullable=False)
+	passwd = Column(String(255), nullable=False)
+	
+
+__all__ = ['Users']
