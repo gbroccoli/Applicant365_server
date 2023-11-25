d9c148f459692c834269b8bcbb7cbdba55ca3f22
Broccoli <vovo.r@ya.ru>
2023-11-26 00:41:36 +1000

    add pydating and add url

--- routers/auth.py
+++ routers/auth.py
@@ -1,6 +1,18 @@
 from fastapi import APIRouter, Form
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from core.config.hashing import PasswordManager
+from pydantic import BaseModel, Field
+from typing import Optional
+import random
+
+class User(BaseModel):
+	id: Optional[int] = None
+	surname: str
+	name: str
+	login: str
+	passwd: str
+
+fake_bd = []
 
 app = APIRouter(
 	tags=["Auth"],
@@ -14,5 +26,9 @@ async def login():
 @app.post("/gets")
 async def hash(password: str = Form(...)):
 	return PasswordManager.hash_password(password)
+
+@app.post("/register")
+async def register(item: User):
+	return item
 
 routers = [app]
