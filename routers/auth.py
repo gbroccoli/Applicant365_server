ffbb8dd32b7a4afb51c7fdfa42b076b51a4b06fd
broccoli <vovo.r@ya.ru>
2023-11-26 14:36:54 +1000

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -4,6 +4,8 @@ from core.config.hashing import PasswordManager
 from pydantic import BaseModel, Field
 from typing import Optional
 import random
+import json
+
 
 class User(BaseModel):
 	id: Optional[int] = None
@@ -12,6 +14,7 @@ class User(BaseModel):
 	login: str
 	passwd: str
 
+
 fake_bd = []
 
 app = APIRouter(
@@ -19,16 +22,33 @@ app = APIRouter(
 	prefix="/auth"
 )
 
+
 @app.get("/login")
 async def login():
 	pass
+
 
 @app.post("/gets")
 async def hash(password: str = Form(...)):
 	return PasswordManager.hash_password(password)
 
-@app.post("/register")
-async def register(item: User):
-	return item
+@app.post("/verify_password")
+async def verify_password(password: str = Form(...)):
+	return PasswordManager.verify_password(password, PasswordManager.hash_password(password))
+
+# @app.post("/register")
+# async def register(surname: str = Form(...), name: str = Form(...), login: str = Form(...), password: str = Form(...)):
+# 	new_id = random.randint(1, 100)
+# 	new_user = {
+# 		"id": new_id,
+# 		"surname": surname,
+# 		"name": name,
+# 		"login": login,
+# 		"password": PasswordManager.hash_password(password)
+# 	}
+
+# 	fake_bd.append(new_user)  # Добавляем нового пользователя в список
+
+# 	return fake_bd  # Возвращаем только что добавленного пользователя
 
 routers = [app]
